6da571ea250f45465208d1ef9cfcc13e5fd57faa
ElJoacoopy <jarcezaracho@gmail.com>
2026-06-05 16:15:10 -0300

    Update archivos.py

--- archivos.py
+++ archivos.py
@@ -7,3 +7,10 @@ def cargarEnArchivo(nombre,texto):
     archivo=open(nombre,"a")
     archivo.write(texto)
     archivo.close()
+def cuentaLineas(nombre): #Sirve para ver si todos los equipos están cargados o no.
+    archivo=open(nombre,"a")
+    lineaCnt=0
+    for line in archivo:
+        lineaCnt+=1
+    archivo.close()
+    return lineaCnt
