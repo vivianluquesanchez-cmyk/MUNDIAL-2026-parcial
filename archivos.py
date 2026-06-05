282163a5b57d8f13e09880b6cf36e4ba21e9588a
ElJoacoopy <jarcezaracho@gmail.com>
2026-06-05 16:10:13 -0300

    Update archivos.py

--- archivos.py
+++ archivos.py
@@ -3,4 +3,7 @@ def crear_archivo(nombre="torneo.txt"):
     if not os.path.exists(nombre):
        archivo = open(nombre, "a")
        archivo.close()
-  
+def cargarEnArchivo(nombre,texto):
+    archivo=open(nombre,"a")
+    archivo.write(texto)
+    archivo.close()
